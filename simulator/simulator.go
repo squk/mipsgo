a2d21ec18e63eac55c3bbaf7bdc829c21166f085
Christian Nieves <me@christiannieves.com>
2017-04-03 18:53:44 -0600

    Add lexing to simulator

--- simulator/simulator.go
+++ simulator/simulator.go
@@ -7,16 +7,34 @@ import (
 
 type Simulator struct {
 	Filename string
-	Source   string
+	Source   []byte
+	Lexer    Lexer
 	VM       VirtualMachine
+}
+
+func NewSimulator(src string) Simulator {
+	var s Simulator
+	s.Filename = ""
+	s.Source = []byte(src)
+	s.Lexer = NewLexer()
+	s.Lexer.Raw = s.Source
+
+	return s
 }
 
 func ReadSource(filename string) Simulator {
 	var s Simulator
 	s.Filename = filename
 	s.GetSource()
+	s.Lexer = NewLexer()
+	s.Lexer.Raw = s.Source
 
 	return s
+}
+
+func (s *Simulator) Run() {
+	s.Lexer.Lex()
+	s.Lexer.PrintTokens()
 }
 
 func (s *Simulator) GetSource() {
