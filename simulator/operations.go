a7dce498c7da15fea8fb54b11c4b26d529d1ca1b
Christian Nieves <me@christiannieves.com>
2017-04-03 18:52:56 -0600

    Add prototype functions for operations

---
+++ simulator/operations.go
@@ -0,0 +1,13 @@
+package simulator
+
+func (vm *VirtualMachine) add(dest, r1, r2 int) {
+
+}
+
+func (vm *VirtualMachine) sub(dest, r1, r2 int) {
+
+}
+
+func (vm *VirtualMachine) slt(dest, r1, r2 int) {
+
+}
