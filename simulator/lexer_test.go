b3f17532c711597b82dcf2a67935323c86d7d216
Christian Nieves <me@christiannieves.com>
2017-04-03 19:17:54 -0600

    Add test case for skipping comments

--- simulator/lexer_test.go
+++ simulator/lexer_test.go
@@ -22,4 +22,10 @@ var _ = Describe("Lexer", func() {
 		Expect(sim.Lexer.Tokens[5].Category).To(Equal(TEXT)) // 't1'
 		Expect(sim.Lexer.Tokens[8].Category).To(Equal(TEXT)) // 't2'
 	})
+
+	It("skips comments", func() {
+		sim = NewSimulator("# this is a test comment")
+		sim.Run()
+		Expect(len(sim.Lexer.Tokens)).To(Equal(0))
+	})
 })
