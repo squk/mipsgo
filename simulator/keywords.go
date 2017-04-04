1644c7adf53c9fde3f0fbaf3345c968967b0e356
Christian Nieves <me@christiannieves.com>
2017-04-03 18:52:36 -0600

    Add MIPS keywords as slice

---
+++ simulator/keywords.go
@@ -0,0 +1,9 @@
+package simulator
+
+var Keywords = [...]string{
+	"add", "addi", "addiu", "addu", "and", "andi", "beq", "bgez", "bgezal",
+	"bgtz", "blez", "bltz", "bltzal", "bne", "div", "divu", "j", "jal", "jr",
+	"lb", "lui", "lw", "mfhi", "mflo", "mult", "multu", "noop", "or", "ori",
+	"sb", "sll", "sllv", "slt", "slti", "sltiu", "sltu", "sra", "srl", "srlv",
+	"sub", "subu", "sw", "syscall", "xor", "xori",
+}
