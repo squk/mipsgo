e5417b6fba27fa9a27440ba7ff1a7415097765ce
Christian Nieves <me@christiannieves.com>
2017-04-05 12:35:36 -0600

    Add test case for SLT operation

--- simulator/simulator_test.go
+++ simulator/simulator_test.go
@@ -10,7 +10,7 @@ import (
 var _ = Describe("Simulator", func() {
 	var sim Simulator
 
-	It("performs addition", func() {
+	It("performs ADD op", func() {
 		sim = NewSimulator(`add $t0, $0, 5
 							add $t1, $t0, 10`)
 		sim.Run()
@@ -18,11 +18,23 @@ var _ = Describe("Simulator", func() {
 		Expect(*(sim.VM.GetReg("t1"))).To(Equal(int32(15)))
 	})
 
-	It("performs subtraction", func() {
+	It("performs SUB op", func() {
 		sim = NewSimulator(`add $t0, $0, 15
 							sub $t1, $t0, 10`)
 		sim.Run()
 		Expect(*(sim.VM.GetReg("t0"))).To(Equal(int32(15)))
 		Expect(*(sim.VM.GetReg("t1"))).To(Equal(int32(5)))
+	})
+
+	It("performs SLT OP", func() {
+		sim = NewSimulator(`add $t0, $0, 5
+							add $t1, $0, 9
+							slt $t2, $t0, $t1
+							slt $t3, $t1, $t2`)
+		sim.Run()
+		Expect(*(sim.VM.GetReg("t0"))).To(Equal(int32(5)))
+		Expect(*(sim.VM.GetReg("t1"))).To(Equal(int32(9)))
+		Expect(*(sim.VM.GetReg("t2"))).To(Equal(int32(1)))
+		Expect(*(sim.VM.GetReg("t3"))).To(Equal(int32(0)))
 	})
 })
