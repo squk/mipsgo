2a31814c44fdca087603f4014ca65013f9b6a04c
Christian Nieves <me@christiannieves.com>
2017-04-05 12:41:02 -0600

    Fix misuse of ADD op and add ADDI test cases

--- simulator/simulator_test.go
+++ simulator/simulator_test.go
@@ -10,25 +10,37 @@ import (
 var _ = Describe("Simulator", func() {
 	var sim Simulator
 
-	It("performs ADD op", func() {
-		sim = NewSimulator(`add $t0, $0, 5
-							add $t1, $t0, 10`)
+	It("performs ADDI op", func() {
+		sim = NewSimulator(`addi $t0, $0, 5
+							addi $t1, $t0, 10`)
 		sim.Run()
 		Expect(*(sim.VM.GetReg("t0"))).To(Equal(int32(5)))
 		Expect(*(sim.VM.GetReg("t1"))).To(Equal(int32(15)))
 	})
 
-	It("performs SUB op", func() {
-		sim = NewSimulator(`add $t0, $0, 15
-							sub $t1, $t0, 10`)
+	It("performs ADD op", func() {
+		sim = NewSimulator(`addi $t0, $0, 15
+							addi $t1, $0, 14
+							add $t2, $t0, $t1`)
 		sim.Run()
 		Expect(*(sim.VM.GetReg("t0"))).To(Equal(int32(15)))
-		Expect(*(sim.VM.GetReg("t1"))).To(Equal(int32(5)))
+		Expect(*(sim.VM.GetReg("t1"))).To(Equal(int32(14)))
+		Expect(*(sim.VM.GetReg("t2"))).To(Equal(int32(29)))
+	})
+
+	It("performs SUB op", func() {
+		sim = NewSimulator(`addi $t0, $0, 15
+							addi $t1, $0, 14
+							sub $t2, $t0, $t1`)
+		sim.Run()
+		Expect(*(sim.VM.GetReg("t0"))).To(Equal(int32(15)))
+		Expect(*(sim.VM.GetReg("t1"))).To(Equal(int32(14)))
+		Expect(*(sim.VM.GetReg("t2"))).To(Equal(int32(1)))
 	})
 
 	It("performs SLT OP", func() {
-		sim = NewSimulator(`add $t0, $0, 5
-							add $t1, $0, 9
+		sim = NewSimulator(`addi $t0, $0, 5
+							addi $t1, $0, 9
 							slt $t2, $t0, $t1
 							slt $t3, $t1, $t2`)
 		sim.Run()
